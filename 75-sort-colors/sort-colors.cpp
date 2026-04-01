cc104be714d946128856368186f120f75dcd3506
shikshapandey201460 <shikshapandey2004@gmail.com>
2026-04-01 13:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.34%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+
+        for( int i=1;i<n;i++){
+            //for round 1 to n-1
+            for(int j=0;j<n-i;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+
+        }
+        
+    }
+};
