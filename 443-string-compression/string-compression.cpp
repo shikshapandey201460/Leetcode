ae092b8f08b46a7e32b4d2b2acb15f5391a5bc26
shikshapandey201460 <shikshapandey2004@gmail.com>
2026-04-08 00:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (45.92%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i=0;
+        int ansIndex=0;
+        int n=chars.size();
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i]==chars[j]){
+                j++;
+            }
+            //yaha kab aoge
+            //ya toh vector pura traverse kr diya
+            //ya fir new/Different character encounter hoga
+            //old character store kr lo
+
+            chars[ansIndex++]=chars[i];
+            int count=j-i;
+            if(count>1){
+                //converting counting into singke digit ans saving in answer
+                string cnt=to_string(count);
+                for(char ch:cnt){
+                    chars[ansIndex++]=ch;
+                }
+            }
+            i=j;
+        }
+
+
+        return ansIndex;
+    }
+};
